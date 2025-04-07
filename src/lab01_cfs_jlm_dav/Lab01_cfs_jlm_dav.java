322d16421ee417b3f98c96ff70f4aec23b40ad16
Javier Leguizamo <jlegui745@gmail.com>
2025-04-07 01:51:02 -0500

    Fix logicas y refactorizacion de variables

--- src/lab01_cfs_jlm_dav/Lab01_cfs_jlm_dav.java
+++ src/lab01_cfs_jlm_dav/Lab01_cfs_jlm_dav.java
@@ -10,15 +10,17 @@ import java.util.Scanner;
 public class Lab01_cfs_jlm_dav {
 
     public static void main(String[] args) {
-        String name, pFranja, eNombreDia = "", eNombreMes = "", address;
-        int id, op_ct, s1 = 0, s2 = 0, s3 = 0, s4 = 0,
-                c1 = 0, c2 = 0, c3 = 0, c4 = 0, count_product = 0,
-                p1 = 0, p2 = 0, p3 = 0, p4 = 0, d1 = 0, d2 = 0,
-                d3 = 0, d4 = 0, d5 = 0, pDiaSemana, pDiaMes, pMes,
-                pAño, pHora, pMins, pSeg, eDiaSemana = 0, eDiaMes = 0,
+        String nombre, pFranja, eNombreDia = "", eNombreMes = "", direccion;
+        int id, categoria, stock_producto1 = 0, stock_producto2 = 0,
+                stock_producto3 = 0, stock_producto4 = 0, cantidad_producto1 = 0,
+                cantidad_producto2 = 0, cantidad_producto3 = 0,
+                cantidad_producto4 = 0, sumatoria_productos = 0,
+                precio_producto1 = 0, precio_producto2 = 0,
+                precio_producto3 = 0, precio_producto4 = 0, pDiaSemana, pDiaMes,
+                pMes, pAño, pHora, pMins, pSeg, eDiaSemana = 0, eDiaMes = 0,
                 eMes = 0, eAño = 0, eHora = 0, eMins = 0, eSeg = 0,
                 pHoraMilitar = 0, eAñoLargo = 0, eMaxDiasMes = 31;
-        float price = 0;
+        float montoTotal = 0;
 
         Scanner read = new Scanner(System.in);
 
@@ -29,10 +31,7 @@ public class Lab01_cfs_jlm_dav {
         System.out.println("888 Y88b888 d8P  Y8b `Y8bd8P' 888  88888 d8P  Y8b 888 \"88b");
         System.out.println("888  Y88888 88888888   X88K   888    888 88888888 888  888");
         System.out.println("888   Y8888 Y8b.     .d8\"\"8b. Y88b  d88P Y8b.     888  888");
-        System.out.println("888    Y888  \"Y8888  888  888  \"Y8888P88  \"Y8888  888  888 ");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("888    Y888  \"Y8888  888  888  \"Y8888P88  \"Y8888  888  888\n");
         System.out.println(" .d8888b.                    888                                       8888888b.     d8888 ");
         System.out.println("d88P  Y88b                   888                                       888   Y88b   d8P888");
         System.out.println(" \"Y888b.   888  888 .d8888b  888888 .d88b.  88888b.d88b.  .d8888b      888   d88P d8P  888 ");
@@ -42,123 +41,123 @@ public class Lab01_cfs_jlm_dav {
         System.out.println(" \"Y8888P\"   \"Y88888  88888P'  \"Y888 \"Y8888  888  888  888  88888P'     888   T88b      888   ");
         System.out.println("                888     ");
         System.out.println("           Y8b d88P  ");
-        System.out.println("            \"Y88P\"  ");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("            \"Y88P\"\n");
 
         System.out.println("Bienvenido a la tienda NexGen Systems R4");
-        System.out.println("Digite su nombre");
-        name = read.nextLine();
-        System.out.println("Digite su direccion");
-        address = read.nextLine();
-        System.out.println("\nCategorias de nuestros productos");
+        System.out.print("Digite su nombre: ");
+        nombre = read.nextLine();
+
+        System.out.print("Digite su direccion: ");
+        direccion = read.nextLine();
+
+        System.out.println("Categorias de nuestros productos");
         System.out.println("  1- Laptops");
         System.out.println("  2- Smartphones");
         System.out.println("  3- Accesorios");
         System.out.println("  4- Componentes de PC");
-        System.out.println("\nDigite el numero de la caterogia");
-        op_ct = read.nextInt();
+        System.out.print("Digite el numero de la categoria: ");
+        categoria = read.nextInt();
 
         // Switch Catergorias
-        switch (op_ct) {
+        switch (categoria) {
             // Categoria #1 'Laptops'
             case 1:
                 // Se le asigna el stock a cada producto
-                s1 = 5;
-                s2 = 10;
-                s3 = 3;
-                s4 = 12;
+                stock_producto1 = 5;
+                stock_producto2 = 10;
+                stock_producto3 = 3;
+                stock_producto4 = 12;
 
                 // Se le asigna el precio a cada producto
-                p1 = 233000;
-                p2 = 52000;
-                p3 = 123000;
-                p4 = 215000;
+                precio_producto1 = 233000;
+                precio_producto2 = 52000;
+                precio_producto3 = 123000;
+                precio_producto4 = 215000;
 
                 System.out.println("\nDigite la cantidad que quiere comprar de cada producto");
-                System.out.printf("Ultrabook X      - %d (Stock disponible %d)%n", p1, s1);
-                c1 = read.nextInt();
-                System.out.printf("Gaming Z         - %d (Stock disponible %d)%n", p2, s2);
-                c2 = read.nextInt();
-                System.out.printf("Workstation Pro  - %d (Stock disponible %d)%n", p3, s3);
-                c3 = read.nextInt();
-                System.out.printf("Chromebook Y     - %d (Stock disponible %d)%n", p4, s4);
-                c4 = read.nextInt();
+                System.out.printf("Ultrabook X      - %d (Stock disponible %d)%n", precio_producto1, stock_producto1);
+                cantidad_producto1 = read.nextInt();
+                System.out.printf("Gaming Z         - %d (Stock disponible %d)%n", precio_producto2, stock_producto2);
+                cantidad_producto2 = read.nextInt();
+                System.out.printf("Workstation Pro  - %d (Stock disponible %d)%n", precio_producto3, stock_producto3);
+                cantidad_producto3 = read.nextInt();
+                System.out.printf("Chromebook Y     - %d (Stock disponible %d)%n", precio_producto4, stock_producto4);
+                cantidad_producto4 = read.nextInt();
                 break;
 
             // Categoria #2 'Smartphones'
             case 2:
                 // Se le asigna el stock a cada producto
-                s1 = 30;
-                s2 = 15;
-                s3 = 13;
-                s4 = 6;
+                stock_producto1 = 30;
+                stock_producto2 = 15;
+                stock_producto3 = 13;
+                stock_producto4 = 6;
 
                 // Se le asigna el precio a cada producto
-                p1 = 333000;
-                p2 = 350000;
-                p3 = 284490;
-                p4 = 104830;
+                precio_producto1 = 333000;
+                precio_producto2 = 350000;
+                precio_producto3 = 284490;
+                precio_producto4 = 104830;
 
                 System.out.println("\nDigite la cantidad que quiere comprar de cada producto");
-                System.out.printf("Galaxy Ultra   - %d (Stock disponible %d)%n", p1, s1);
-                c1 = read.nextInt();
-                System.out.printf("Iphone Pro     - %d (Stock disponible %d)%n", p2, s2);
-                c2 = read.nextInt();
-                System.out.printf("Pixel Max      - %d (Stock disponible %d)%n", p3, s3);
-                c3 = read.nextInt();
-                System.out.printf("OnePlus Speed  - %d (Stock disponible %d)%n", p4, s4);
-                c4 = read.nextInt();
+                System.out.printf("Galaxy Ultra   - %d (Stock disponible %d)%n", precio_producto1, stock_producto1);
+                cantidad_producto1 = read.nextInt();
+                System.out.printf("Iphone Pro     - %d (Stock disponible %d)%n", precio_producto2, stock_producto2);
+                cantidad_producto2 = read.nextInt();
+                System.out.printf("Pixel Max      - %d (Stock disponible %d)%n", precio_producto3, stock_producto3);
+                cantidad_producto3 = read.nextInt();
+                System.out.printf("OnePlus Speed  - %d (Stock disponible %d)%n", precio_producto4, stock_producto4);
+                cantidad_producto4 = read.nextInt();
                 break;
 
             // Categoria #3 'Accesorios'
             case 3:
                 // Se le asigna el stock a cada producto
-                s1 = 8;
-                s2 = 2;
-                s3 = 7;
-                s4 = 9;
+                stock_producto1 = 8;
+                stock_producto2 = 2;
+                stock_producto3 = 7;
+                stock_producto4 = 9;
 
                 // Se le asigna el precio a cada producto
-                p1 = 50293;
-                p2 = 150830;
-                p3 = 230490;
-                p4 = 59999;
+                precio_producto1 = 50293;
+                precio_producto2 = 150830;
+                precio_producto3 = 230490;
+                precio_producto4 = 59999;
 
                 System.out.println("\nDigite la cantidad que quiere comprar de cada producto");
-                System.out.printf("Audifonos Inalambricos  - %d (Stock disponible %d)%n", p1, s1);
-                c1 = read.nextInt();
-                System.out.printf("Teclado Mecanico        - %d (Stock disponible %d)%n", p2, s2);
-                c2 = read.nextInt();
-                System.out.printf("Mouse Gamer             - %d (Stock disponible %d)%n", p3, s3);
-                c3 = read.nextInt();
-                System.out.printf("Cargador Rapido         - %d (Stock disponible %d)%n", p4, s4);
-                c4 = read.nextInt();
+                System.out.printf("Audifonos Inalambricos  - %d (Stock disponible %d)%n", precio_producto1, stock_producto1);
+                cantidad_producto1 = read.nextInt();
+                System.out.printf("Teclado Mecanico        - %d (Stock disponible %d)%n", precio_producto2, stock_producto2);
+                cantidad_producto2 = read.nextInt();
+                System.out.printf("Mouse Gamer             - %d (Stock disponible %d)%n", precio_producto3, stock_producto3);
+                cantidad_producto3 = read.nextInt();
+                System.out.printf("Cargador Rapido         - %d (Stock disponible %d)%n", precio_producto4, stock_producto4);
+                cantidad_producto4 = read.nextInt();
                 break;
 
             // Categoria #4 'Componentes de PC'
             case 4:
                 // Se le asigna el stock a cada producto
-                s1 = 14;
-                s2 = 20;
-                s3 = 23;
-                s4 = 25;
+                stock_producto1 = 14;
+                stock_producto2 = 20;
+                stock_producto3 = 23;
+                stock_producto4 = 25;
 
                 // Se le asigna el precio a cada producto
-                p1 = 399990;
-                p2 = 370849;
-                p3 = 284930;
-                p4 = 83490;
+                precio_producto1 = 399990;
+                precio_producto2 = 370849;
+                precio_producto3 = 284930;
+                precio_producto4 = 83490;
 
                 System.out.println("\nDigite la cantidad que quiere comprar de cada producto");
-                System.out.printf("Tarjeta Grafica RTX  - %d (Stock disponible %d)%n", p1, s1);
-                c1 = read.nextInt();
-                System.out.printf("Procesador i9        - %d (Stock disponible %d)%n", p2, s2);
-                c2 = read.nextInt();
-                System.out.printf("SSD NVMe             - %d (Stock disponible %d)%n", p3, s3);
-                c3 = read.nextInt();
-                System.out.printf("Fuente de poder      - %d (Stock disponible %d)%n", p4, s4);
-                c4 = read.nextInt();
+                System.out.printf("Tarjeta Grafica RTX  - %d (Stock disponible %d)%n", precio_producto1, stock_producto1);
+                cantidad_producto1 = read.nextInt();
+                System.out.printf("Procesador i9        - %d (Stock disponible %d)%n", precio_producto2, stock_producto2);
+                cantidad_producto2 = read.nextInt();
+                System.out.printf("SSD NVMe             - %d (Stock disponible %d)%n", precio_producto3, stock_producto3);
+                cantidad_producto3 = read.nextInt();
+                System.out.printf("Fuente de poder      - %d (Stock disponible %d)%n", precio_producto4, stock_producto4);
+                cantidad_producto4 = read.nextInt();
                 break;
 
             default:
@@ -167,44 +166,44 @@ public class Lab01_cfs_jlm_dav {
                 return;
         }
 
-        if (c1 < 0) {
+        if (cantidad_producto1 < 0) {
             System.out.println("La cantidad solicitada del 1er producto es invalida");
-        } else if (c1 > s1) {
+        } else if (cantidad_producto1 > stock_producto1) {
             System.out.println("La cantidad solicitada del 1er producto no esta disponible");
         } else {
-            count_product += c1;
-            price = c1 * p1;
-            s1 -= c1;
+            sumatoria_productos += cantidad_producto1;
+            montoTotal = cantidad_producto1 * precio_producto1;
+            stock_producto1 -= cantidad_producto1;
         }
 
-        if (c2 < 0) {
+        if (cantidad_producto2 < 0) {
             System.out.println("La cantidad solicitada del 2do producto es invalida");
-        } else if (c2 > s2) {
+        } else if (cantidad_producto2 > stock_producto2) {
             System.out.println("La cantidad solicitada del 2do producto no esta disponible");
         } else {
-            count_product += c2;
-            price = price + (c2 * p2);
-            s2 -= c2;
+            sumatoria_productos += cantidad_producto2;
+            montoTotal = montoTotal + (cantidad_producto2 * precio_producto2);
+            stock_producto2 -= cantidad_producto2;
         }
 
-        if (c3 < 0) {
+        if (cantidad_producto3 < 0) {
             System.out.println("La cantidad solicitada del 3er producto es invalida");
-        } else if (c3 > s3) {
+        } else if (cantidad_producto3 > stock_producto3) {
             System.out.println("La cantidad solicitada del 3er producto no esta disponible");
         } else {
-            count_product += c3;
-            price = price + (c3 * p3);
-            s3 -= c3;
+            sumatoria_productos += cantidad_producto3;
+            montoTotal = montoTotal + (cantidad_producto3 * precio_producto3);
+            stock_producto3 -= cantidad_producto3;
         }
 
-        if (c4 < 0) {
+        if (cantidad_producto4 < 0) {
             System.out.println("La cantidad solicitada del 4to producto es invalida");
-        } else if (c4 > s4) {
+        } else if (cantidad_producto4 > stock_producto4) {
             System.out.println("La cantidad solicitada del 4to producto no esta disponible");
         } else {
-            count_product += c4;
-            price = price + (c4 * p4);
-            s4 -= c4;
+            sumatoria_productos += cantidad_producto4;
+            montoTotal = montoTotal + (cantidad_producto4 * precio_producto4);
+            stock_producto4 -= cantidad_producto4;
         }
 
         // Solicitar fecha y hora
@@ -274,9 +273,10 @@ public class Lab01_cfs_jlm_dav {
             if (pDiaMes < 1 || pDiaMes > eMaxDiasMes) {
                 System.out.println("El valor proporcionado para dia del mes es invalido.");
             } else {
+                pHoraMilitar = pHora;
                 if (pHora == 12 && pFranja.equals("a.m.")) {
                     pHoraMilitar = 0;
-                } else {
+                } else if (pFranja.equals("p.m.")) {
                     pHoraMilitar = (pHora == 12) ? 12 : pHora + 12;
                 }
 
@@ -386,7 +386,7 @@ public class Lab01_cfs_jlm_dav {
         }
 
         // Solicitar y verificar id
-        System.out.println("Digite su ID");
+        System.out.print("Digite su ID: ");
         id = read.nextInt();
 
         if (id < 10000 || id > 99999) {
@@ -397,49 +397,48 @@ public class Lab01_cfs_jlm_dav {
             // Se verifica si el cliente cuenta con los requisitos para el descuento
             // Descuento #1
             if (id % 3 == 0) {
-                if (count_product > 10 && price > 500000) {
-                    price *= .90; // 10%
+                if (sumatoria_productos >= 5 && sumatoria_productos <= 10) {
+                    montoTotal *= .95; // 5%
                 }
 
-                if (count_product >= 5 && count_product <= 10) {
-                    price *= .95; // 5%
+                if (sumatoria_productos > 10 && montoTotal > 500000) {
+                    montoTotal *= .90; // 10%
                 }
             }
 
             // Descuento #2
             // Se decompone el numero para conseguir cada digito
-            int id_t = id;
-            d5 = id_t % 10;
-            id_t = id_t / 10;
-            d4 = id_t % 10;
-            id_t = id_t / 10;
-            d3 = id_t % 10;
-            id_t = id_t / 10;
-            d2 = id_t % 10;
-            id_t = id_t / 10;
-            d1 = id_t % 10;
+            int sumatoria = 0;
+            int temp = id;
+            sumatoria += temp % 10;
+            temp /= 10;
+            sumatoria += temp % 10;
+            temp /= 10;
+            sumatoria += temp % 10;
+            temp /= 10;
+            sumatoria += temp % 10;
+            temp /= 10;
+            sumatoria += temp % 10;
 
-            id_t = d1 + d2 + d3 + d4 + d5;
-
-            if (id_t % 2 != 0) {
-                if (count_product >= 8 && count_product <= 15) {
-                    price *= .93; // 7%
+            if (sumatoria % 2 != 0) {
+                if (sumatoria_productos >= 8 && sumatoria_productos <= 15) {
+                    montoTotal *= .93; // 7%
                 }
 
-                if (count_product > 20 && price > 1000000) {
-                    price *= .85; // 15%
+                if (sumatoria_productos > 20 && montoTotal > 1000000) {
+                    montoTotal *= .85; // 15%
                 }
             }
         }
 
-        price *= 1.19;
+        montoTotal *= 1.19;
 
-        System.out.println("\n\nConfirmacion de pedido ------");
-        System.out.printf("%s\n%s\n", name, address);
+        System.out.println("\nConfirmacion de pedido ------");
+        System.out.printf("%s (#%s) \n%s \n", nombre, id, direccion);
         System.out.println("Fecha de entrega");
-        System.out.printf("%s, %d de %s del %d%n", eNombreDia, eDiaMes, eNombreMes, eAñoLargo, eHora, eMins, eSeg);
+        System.out.printf("%s, %d de %s del %d \n", eNombreDia, eDiaMes, eNombreMes, eAñoLargo, eHora, eMins, eSeg);
         System.out.println("Hora maxima de entrega");
-        System.out.printf("%d%d horas%n", eHora, eMins);
-        System.out.printf("Valor de la compra incluido el IVA es de: %.2f%n", price);
+        System.out.printf("%02d:%02d:%02d horas \n", eHora, eMins, eSeg);
+        System.out.printf("Valor de la compra incluido el IVA es de: %.2f \n", montoTotal);
     }
 }
